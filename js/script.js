920b2a6453605ce55e54ebecadb85431e3458b86
Maks <maxim.bezliepkin@gmail.com>
2021-01-13 23:17:22 +0200

    Added the prompt function in JS

--- js/script.js
+++ js/script.js
@@ -1,25 +1,35 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+// const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
-const obj = {
-    name: "Maks",
-    age: 27,
-    isMarried: true
-};
+// const obj = {
+//     name: "Maks",
+//     age: 27,
+//     isMarried: true
+// };
 
-console.log(obj.isMarried);
+// console.log(obj.isMarried);
 
 // alert('You are studying now!');
 // const result = confirm(`Are you here?`);
 // console.log(result);
-const answer = [];
+// const answer = [];
 
-answer[0] = prompt("Hi, what's your NAME?", "name");
-answer[1] = prompt("What's your SECOND NAME?", "second name");
-answer[2] = prompt("What's your THIRD NAME?", "third name");
-document.write(answer);
+// answer[0] = prompt("Hi, what's your NAME?", "name");
+// answer[1] = prompt("What's your SECOND NAME?", "second name");
+// answer[2] = prompt("What's your THIRD NAME?", "third name");
+// document.write(answer);
+
+// <<<<>>>>>
+// let a = 8;
+// let b = '4';
+// console.log(a/2 == b);
+const isChecked = false,
+      isClosed = false;
+console.log(isChecked || isClosed);
+question = prompt("Are you OK?", "no");
+
